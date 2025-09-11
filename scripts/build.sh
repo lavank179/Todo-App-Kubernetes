59ceb63a9b9f9112bdebe34f09d8c21794f1d3c1
lavan179 <chlavankumar179@gmail.com>
2025-09-11 21:08:28 +0530

    use docker login in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,10 @@
 
 ls -ltr && cd frontend && docker build -t ${DOCKER_IMAGE} .
 docker images | grep ${DOCKER_IMAGE}
+
+echo "user: $REGISTRY_CREDENTIALS_Username"
+echo "user: $REGISTRY_CREDENTIALS_Password"
+
+echo "$REGISTRY_CREDENTIALS_Password" | docker login --username "$REGISTRY_CREDENTIALS_Username" --password-stdin
 docker push ${DOCKER_IMAGE}
 echo "Pushed successfully!"
