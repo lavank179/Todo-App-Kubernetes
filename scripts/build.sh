3109ce71f50ad65839ccf413ca92149b7a7e3ab3
lavan179 <chlavankumar179@gmail.com>
2025-09-11 22:15:33 +0530

    use build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@
 ls -ltr && cd frontend && docker build -t ${DOCKER_IMAGE} .
 docker images | grep ${DOCKER_IMAGE}
 
-echo "user: $REGISTRY_CREDENTIALS_Username"
+echo "user: $REGISTRY_CREDENTIALS"
 echo "user: $REGISTRY_CREDENTIALS_Password"
 
 echo "$REGISTRY_CREDENTIALS_Password" | docker login --username "$REGISTRY_CREDENTIALS_Username" --password-stdin
