d27e8588ca5da0aff3a461bbd231521df615335c
lavan179 <chlavankumar179@gmail.com>
2025-09-11 23:04:03 +0530

    use build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+set -eo
 
 ls -ltr && cd frontend && docker build -t ${DOCKER_IMAGE} .
 docker images | grep ${DOCKER_IMAGE}
 
-echo "user: $DOCKER_USER"
-echo "user: $DOCKER_PASS"
-
+docker images --format '{{.Repository}}:{{.Tag}}' --filter=reference='lavank179/todo-k8s-ui' | sort -t: -k2 -n | head -n -2 | xargs -r docker rmi
 echo "$DOCKER_PASS" | docker login --username "$DOCKER_USER" --password-stdin
 docker push ${DOCKER_IMAGE}
 echo "Pushed successfully!"
+
