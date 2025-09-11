3b01d14dd4c64da0dd4c84727edfb58b39441b06
lavan179 <chlavankumar179@gmail.com>
2025-09-11 21:01:22 +0530

    use build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ls -ltr && cd frontend && docker build -t ${DOCKER_IMAGE} .
-docker images | grep ${ DOCKER_IMAGE }
-docker push ${ DOCKER_IMAGE }
+docker images | grep ${DOCKER_IMAGE}
+docker push ${DOCKER_IMAGE}
 echo "Pushed successfully!"
