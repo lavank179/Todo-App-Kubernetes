0975c44eaf5d97f43cf3917925b2aa2ab9b70793
lavank179 <chlavankumar179@gmail.com>
2026-04-12 13:17:52 +0530

    actions pipeline phase1 - build containers

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,40 +1,41 @@
 #!/bin/bash
 set -eox
 
-IsBuildAndPush="${1:-false}"
-BNUMBER="${2:-0}"
+TASK="${1}"
+IMAGEREPO="${IMAGE_REPO}"
+BUILDNUMBER="${BUILD_NUMBER}"
 
-function buildAndPushImage() {
-  DOCKER_IMAGE="$1:$BUILD_NUMBER"
-  docker build -t ${DOCKER_IMAGE} .
+# function buildAndPushImage() {
+#   DOCKER_IMAGE="$1:$BUILD_NUMBER"
+#   docker build -t ${DOCKER_IMAGE} .
 
-  docker images --format '{{.Repository}}:{{.Tag}}' --filter=reference="$1" | sort -t: -k2 -n | head -n -2 | xargs -r docker rmi
-  echo "$DOCKER_PASS" | docker login --username "$DOCKER_USER" --password-stdin
-  docker push ${DOCKER_IMAGE}
+#   docker images --format '{{.Repository}}:{{.Tag}}' --filter=reference="$1" | sort -t: -k2 -n | head -n -2 | xargs -r docker rmi
+#   echo "$DOCKER_PASS" | docker login --username "$DOCKER_USER" --password-stdin
+#   docker push ${DOCKER_IMAGE}
+# }
+
+function buildContainers() {
+  ls -l
+  cd frontend
+  docker build -t "$IMAGEREPO/frontend:$BUILDNUMBER" .
+  cd ../backend
+  docker build -t "$IMAGEREPO/backend:$BUILDNUMBER" .
+  echo "Containers built successfully!"
 }
 
-function buildOnlyImage() {
-  BUILD_NUMBER=$2
-  DOCKER_IMAGE="$1:$BUILD_NUMBER"
-  docker build -t ${DOCKER_IMAGE} .
+function testContainers() {
+  echo "Testing containers..."
+  # Here you can add commands to run your tests against the built containers
+  # For example, you might use docker run to start the containers and then execute test scripts
+  docker images
+  echo "Tests completed successfully!"
 }
 
-ls -ltr
-cd frontend
-if [[ "$IsBuildAndPush" == "true" ]]; then
-  buildAndPushImage "lavank179/todo-k8s-ui"
+if [[ "$TASK" == "build_containers" ]]; then
+  buildContainers
+elif [[ "$TASK" == "test_containers" ]]; then
+  testContainers
 else
-  buildOnlyImage "lavank179/todo-k8s-ui" $BNUMBER
+  echo "Unknown argument for task: $TASK, select either 'build_containers' or 'test_containers'"
+  exit 1
 fi
-cd ..
-
-cd backend
-if [[ "$IsBuildAndPush" == "true" ]]; then
-  buildAndPushImage "lavank179/todo-k8s-api"
-else
-  buildOnlyImage "lavank179/todo-k8s-api" $BNUMBER
-fi
-cd ..
-
-echo "Pushed successfully!"
-
